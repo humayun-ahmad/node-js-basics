25f3edf6904f1f3b68e9f20573826ee0260421cc
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-10-25 17:37:23 +0600

    some change in those file

--- w3 School tutorial/demo_readfile.js
+++ w3 School tutorial/demo_readfile.js
@@ -1,3 +1,4 @@
+
 // require is a function which help to import module in a file
 var http = require('http');
 //To include the file System module, use the require() method;
@@ -6,9 +7,10 @@ var fs = require('fs');
 // create a server object:
 http.createServer(function (req,res) {
 // Open a file on the server and return its content:
-  fs.readFile('demofile1.html', function (err,data) {
-    res.writeHead(200,{'Content-Type':'text/html'});
-    res.write(data);
-    return res.end();
+  fs.readFile('demofile1.html','utf8' function (err,data) {
+      console.log(data);
   });
+  // res.writeHead(200,{'Content-Type':'text/html'});
+
+  // return res.end();
 }).listen(8080); // the server object listens on port 3000
