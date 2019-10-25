7297fb295b979733bc5db5ed810877cce210ecd8
Humayun Ahmad Rajib <humayun.ahmad.rajib@gmail.com>
2019-10-25 20:36:05 +0600

    event emitter

--- eventEmitter.js
+++ eventEmitter.js
@@ -5,13 +5,15 @@ var events = require('events');
 var eventEmitrer = new events.EventEmitter();
 
 // To fire an event, use the emit() method:
-// Create an event handler:
-var myEventHander = function() {
+// Create an event handler with function expression:
+var myEventHander = function(msg) {
   console.log("I hear a scream!");
+  console.log(msg);
 }
 
 // Assign the event handler to an event:
 eventEmitrer.on('scream', myEventHander);
 // console.log(a);
 // Fire the 'scream' event:
-eventEmitrer.emit('scream');
+eventEmitrer.emit('scream'); //both are right
+eventEmitrer.emit('scream','the event was emitted');
