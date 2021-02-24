655f09623a5192541d1380ca8f4d3158918e4e17
humayun-ahmad <humayunraj789@gmail.com>
2021-02-24 23:15:56 +0600

    javascript file added

---
+++ script.js
@@ -0,0 +1,72 @@
+/*
+var firstName = 'John';
+console.log(firstName);
+
+var lastName = 'Smith';
+
+var age = 28;
+
+var fullAge = true;
+console.log
+
+var job;
+console.log(job);
+*/
+
+/*******************
+* coding chalange 1
+*/
+
+
+
+/*
+var markMass = prompt("Enter Mark's mass & and it's unit will be kg.");
+var markHeight = prompt("Enter Mark's height & it's unit will be meter");
+var johnMass = prompt("Enter John's mass & it's unit will be kg ");
+var markHeight = prompt("Enter John's height & it's unit will be meter. ");
+
+var markBMI, johnBMI;
+
+markBMI = markMass/(markHeight*markHeight);
+johnBMI = johnMass/(johnMass*johnMass);
+
+Markishigher = markBMI > johnBMI;
+
+alert("Is Mark's BMI higer than John's? " + Markishigher);
+*/
+
+
+
+/*****************************************
+* coding chalange 3
+*/
+
+var bills = [124,48,268];
+
+function tipCalculator(tipPercent, bill){
+    var tip = (tipPercent/100)*bill;
+    return tip;
+}
+
+var tips = [tipCalculator(15,124) + bills[0],tipCalculator(20,48)+bills[1] ,tipCalculator(10,268) +bills[2] ];
+
+console.log(bills);
+console.log(tips);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
